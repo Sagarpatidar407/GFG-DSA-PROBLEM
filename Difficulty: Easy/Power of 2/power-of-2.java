d6490087a2fddf2da064b0ede93473e3e7df1e63
sagar patidar <152028847+Sagarpatidar407@users.noreply.github.com>
2026-06-22 14:40:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Power of 2/power-of-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static boolean isPowerofTwo(int n) {
+        // code here
+        // if(n<=0) return false;
+        
+        // while(n%2==0){
+        //     n = n/2;
+        // }
+        
+        // return n==1;
+        
+        return (n>0) && (n & (n-1)) == 0;
+        
+    }
+}
